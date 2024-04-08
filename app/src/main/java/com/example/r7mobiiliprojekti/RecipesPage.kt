ff1f5e6af44d457ab2221839632b9b9377672b34
Santeri <122432859+Peasami@users.noreply.github.com>
2024-04-08 14:46:09 +0300

    Show response to user

--- app/src/main/java/com/example/r7mobiiliprojekti/RecipesPage.kt
+++ app/src/main/java/com/example/r7mobiiliprojekti/RecipesPage.kt
@@ -5,10 +5,12 @@ import android.os.PersistableBundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,12 +24,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
 import com.aallam.openai.api.chat.chatCompletionRequest
@@ -37,13 +42,36 @@ import com.aallam.openai.client.OpenAI
 import com.example.r7mobiiliprojekti.ui.theme.R7MobiiliprojektiTheme
 import kotlinx.coroutines.launch
 
+var response: String = ""
+
 @Composable
 fun RecipesPage() {
-    // val itemList = listOf("Banana", "Tomato", "EGGS", "Milk", "Pepsi","bread")
-    // ItemList(itemList = itemList)
+    // Returns a scope that's cancelled when RecipeButton is removed from composition
+    val coroutineScope = rememberCoroutineScope()
 
-    RecipeButton()
+    var recipeText by remember {
+        mutableStateOf("")
+    }
 
+    var recipeVisible by remember {
+        mutableStateOf(false)
+    }
+
+    val createRecipeOnClick: () -> Unit = {
+        coroutineScope.launch {
+            recipeText = createMessage("Hey")
+            recipeVisible = true
+            Log.d("chat message", response)
+        }
+    }
+
+
+
+    RecipeButton(onClick = createRecipeOnClick)
+
+    if (recipeVisible){
+        ResponseCard(text = recipeText, onClick = {recipeVisible = false})
+    }
 }
 
 private suspend fun createMessage(request: String) : String{
@@ -70,26 +98,20 @@ private suspend fun createMessage(request: String) : String{
     return message
 }
 
+fun test() {
+
+}
+
 @Composable
-fun RecipeButton() {
-    // Returns a scope that's cancelled when RecipeButton is removed from composition
-    val coroutineScope = rememberCoroutineScope()
-    var response: String = ""
+fun RecipeButton(onClick: () -> Unit) {
 
-    val createRecipeOnClick: () -> Unit = {
-        coroutineScope.launch {
-            response= createMessage("Hey")
-            Log.d("chat message", response)
-        }
-    }
-
-    Button(onClick = createRecipeOnClick) {
+    Button(onClick = onClick) {
         Text("Create Recipe")
     }
 }
 
 @Composable
-fun ResponseCard (text: String) {
+fun ResponseCard (text: String, onClick: () -> Unit) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     Box(
@@ -109,6 +131,16 @@ fun ResponseCard (text: String) {
             Text(
                 text = text,
                 style = MaterialTheme.typography.headlineMedium,
+            )
+        }
+        Button(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(end = 36.dp, bottom = 36.dp),
+            onClick = onClick
+        ) {
+            Text(
+                text = "Close"
             )
         }
     }
@@ -141,8 +173,10 @@ fun ItemCard(item: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 private fun ItemCardPreview() {
-    ItemList(listOf("Banana", "Tomato", "EGGS"))
+    ResponseCard(text = "hello") {
+        
+    }
 }
